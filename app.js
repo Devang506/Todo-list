7703ef0300691b0f5a6eb2e056e5034b08564b57
devangjoshi <devangjoshi1210@gmail.com>
2022-07-04 22:24:31 +0530

    second commit

--- app.js
+++ app.js
@@ -190,6 +190,6 @@ app.post("/delete",function(req,res){
 
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(3000||process.env.PORT, function() {
+  console.log("Server started succesfully");
 });
